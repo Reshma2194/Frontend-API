4064f49eb71a8d185234df816e7326aae82120a3
Reshma <nairreshmam@gmail.com>
2022-05-31 12:37:18 +0530

    updated version - 31-05-2022

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
 let express = require("express");
+let fs = require("fs");
+let cookieparser = require("cookie-parser");
 let app = express();
 
 app.use(express.static("public"));
+app.use(cookieparser());
 
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/pages/index.html");
+    res.end();
 });
 
 app.get("/login", (req, res)=>{
     res.sendFile(__dirname + "/pages/login.html");
+});
+
+app.get("/admin/courses", (req, res)=>{
+    if(req.cookies.usertype != "admin")
+    {
+        res.redirect("../login");        
+    }
+    else{
+        res.write(fs.readFileSync(__dirname + "/pages/admin/header.html"));
+        res.write(fs.readFileSync(__dirname + "/pages/admin/courses.html"));
+        res.write(fs.readFileSync(__dirname + "/pages/admin/footer.html"));
+        res.end();
+    }
+});
+
+
+app.get("/admin/course", (req, res)=>{
+    if(req.cookies.usertype != "admin")
+    {
+        res.redirect("../login");
+    }
+    res.write(fs.readFileSync(__dirname + "/pages/admin/header.html"));
+    res.write(fs.readFileSync(__dirname + "/pages/admin/course.html"));
+    res.write(fs.readFileSync(__dirname + "/pages/admin/footer.html"));
+    res.end();
+});
+
+
+app.get("/admin/logout", (req, res)=>{
+    res.cookie('usertype', '', { maxAge: -900000, httpOnly: true });
+    res.cookie('id', '', { maxAge: -900000, httpOnly: true });
+    res.cookie('name', '', { maxAge: -900000, httpOnly: true });
+    res.cookie('email', '', { maxAge: -900000, httpOnly: true });
+    res.redirect("../login");
 });
 
 app.listen(8085, ()=>{
